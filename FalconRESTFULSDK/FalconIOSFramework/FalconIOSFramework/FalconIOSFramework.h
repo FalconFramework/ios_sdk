7730632541a3d4cfa74dd3e40d7269037c00249a
thiagotmb <tmb0710@gmail.com>
2016-06-05 21:31:33 -0300

    Adding .gitignore and project

---
+++ FalconRESTFULSDK/FalconIOSFramework/FalconIOSFramework/FalconIOSFramework.h
@@ -0,0 +1,13 @@
+//
+//  FalconIOSFramework.h
+//  FalconIOSFramework
+//
+//  Created by Thiago-Bernardes on 6/5/16.
+//  Copyright © 2016 Falcon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FalconIOSFramework : NSObject
+
+@end
---
+++ FalconRestfulSDK/FalconRestfulSDK/FalconRestfulSDK/FalconRestfulSDK.h
@@ -0,0 +1,19 @@
+//
+//  FalconRestfulSDK.h
+//  FalconRestfulSDK
+//
+//  Created by Thiago-Bernardes on 6/5/16.
+//  Copyright © 2016 Falcon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FalconRestfulSDK.
+FOUNDATION_EXPORT double FalconRestfulSDKVersionNumber;
+
+//! Project version string for FalconRestfulSDK.
+FOUNDATION_EXPORT const unsigned char FalconRestfulSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FalconRestfulSDK/PublicHeader.h>
+
+
